449e88e924c7566dd012a23d77b152922b546892
Johntavious Barkley <jbarkley3@gatech.edu>
2016-11-30 20:18:15 -0500

    Stuff

--- map_sunchart.js
+++ map_sunchart.js
@@ -114,7 +114,7 @@ const draw_circles = (data, city_frequency) => {
 }
 
 var map_frequency_to_radius = function(city, frequency) {
-  return view_country ? frequency[city] : 1
+  return 5 * Math.sqrt(frequency[city]/Math.PI)
 }
 
 function clicked(d) {
