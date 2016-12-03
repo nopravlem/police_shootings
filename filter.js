35460d68091dd79601dea4fccf1434976d0ffc33
Lowery, Marcelles L'von <mlowery30@gatech.edu>
2016-12-03 13:55:54 -0500

    Cleaning up
    Removing unnecessary files and organizing data

--- filter.js
+++ filter.js
@@ -2,15 +2,10 @@ d3.csv("locations.csv", function(data) {
 	var dataset = data;
 	// console.log(dataset);
 
-	//Female-->Asian
+	//Memale-->Asian
 	var afiltered_data = dataset.filter(function (d) {
-		if((d["gender"] == "M") && (d["race"] == "Asian") && (d["body_camera"] == "FALSE"))  { // "FALSE"  "TRUE"
-			var age = +d["age"];
-			if (50 <= age && age >= 59){
-				return true;
-			} else {
-				return false;
-			}
+		if((d["gender"] == "M") && (d["race"] == "Asian") && (d["body_camera"] == "FALSE") && (d["age"] == "NULL"))  { // "FALSE"  "FALSE"
+			return true;
 		} else {
 			return false;
 		}
@@ -18,15 +13,10 @@ d3.csv("locations.csv", function(data) {
 
 	console.log(afiltered_data);
 
-	//Female-->Black
+	//Memale-->Black
 	var bfiltered_data = dataset.filter(function (d) {
-		if((d["gender"] == "M") && (d["race"] == "Black") && (d["body_camera"] == "FALSE"))  {
-			var age = +d["age"];
-			if (50 <= age && age >= 59){
-				return true;
-			} else {
-				return false;
-			}
+		if((d["gender"] == "M") && (d["race"] == "Black") && (d["body_camera"] == "FALSE") && (d["age"] == "NULL"))  { // "FALSE"  "FALSE"
+			return true;
 		} else {
 			return false;
 		}
@@ -34,15 +24,10 @@ d3.csv("locations.csv", function(data) {
 
 	console.log(bfiltered_data);
 
-	//Female-->Hispanic
+	//Memale-->Hispanic
 	var cfiltered_data = dataset.filter(function (d) {
-		if((d["gender"] == "M") && (d["race"] == "Hispanic") && (d["body_camera"] == "FALSE"))  {
-			var age = +d["age"];
-			if (50 <= age && age >= 59){
-				return true;
-			} else {
-				return false;
-			}
+		if((d["gender"] == "M") && (d["race"] == "Hispanic") && (d["body_camera"] == "FALSE") && (d["age"] == "NULL"))  { // "FALSE"  "FALSE"
+			return true;
 		} else {
 			return false;
 		}
@@ -50,15 +35,10 @@ d3.csv("locations.csv", function(data) {
 
 	console.log(cfiltered_data);
 
-	//Female-->Native American
+	//Memale-->Native American
 	var dfiltered_data = dataset.filter(function (d) {
-		if((d["gender"] == "M") && (d["race"] == "Native American") && (d["body_camera"] == "FALSE"))  {
-			var age = +d["age"];
-			if (50 <= age && age >= 59){
-				return true;
-			} else {
-				return false;
-			}
+		if((d["gender"] == "M") && (d["race"] == "Native American") && (d["body_camera"] == "FALSE") && (d["age"] == "NULL"))  { // "FALSE"  "FALSE"
+			return true;
 		} else {
 			return false;
 		}
@@ -66,15 +46,10 @@ d3.csv("locations.csv", function(data) {
 
 	console.log(dfiltered_data);
 
-	//Female-->Other
+	//Memale-->Other
 	var efiltered_data = dataset.filter(function (d) {
-		if((d["gender"] == "M") && (d["race"] == "Other") && (d["body_camera"] == "FALSE"))  {
-			var age = +d["age"];
-			if (50 <= age && age >= 59){
-				return true;
-			} else {
-				return false;
-			}
+		if((d["gender"] == "M") && (d["race"] == "Other") && (d["body_camera"] == "FALSE") && (d["age"] == "NULL"))  { // "FALSE"  "FALSE"
+			return true;
 		} else {
 			return false;
 		}
@@ -82,15 +57,21 @@ d3.csv("locations.csv", function(data) {
 
 	console.log(efiltered_data);
 
-	//Female-->White
+	//Memale-->White
 	var ffiltered_data = dataset.filter(function (d) {
-		if((d["gender"] == "M") && (d["race"] == "White") && (d["body_camera"] == "FALSE"))  {
-			var age = +d["age"];
-			if (50 <= age && age >= 59){
-				return true;
-			} else {
-				return false;
-			}
+		if((d["gender"] == "M") && (d["race"] == "White") && (d["body_camera"] == "FALSE") && (d["age"] == "NULL"))  { // "FALSE"  "FALSE"
+			return true;
+		} else {
+			return false;
+		}
+	})
+
+	console.log(ffiltered_data);
+
+	//Memale-->White
+	var ffiltered_data = dataset.filter(function (d) {
+		if((d["gender"] == "M") && (d["race"] == "NULL") && (d["body_camera"] == "FALSE") && (d["age"] == "NULL"))  { // "FALSE"  "FALSE"
+			return true;
 		} else {
 			return false;
 		}
